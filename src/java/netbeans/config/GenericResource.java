b15f2878682bc776370e5730eeeb2ee42edc66a7
Tushar <c0653602@mylambton.ca>
2015-07-25 20:37:10 -0400

    Implementing GenericResources

--- src/java/netbeans/config/GenericResource.java
+++ src/java/netbeans/config/GenericResource.java
@@ -6,50 +6,86 @@
 
 package netbeans.config;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import entity.Product;
+import entity.ProductList;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * REST Web Service
  *
  * @author c0653602
  */
-@Path("generic")
+@Path("/products")
+@RequestScoped
 public class GenericResource {
-
-    @Context
-    private UriInfo context;
-
-    /**
-     * Creates a new instance of GenericResource
-     */
-    public GenericResource() {
-    }
-
-    /**
-     * Retrieves representation of an instance of netbeans.config.GenericResource
-     * @return an instance of java.lang.String
-     */
+@Inject
+    ProductList productList;
+    
     @GET
-    @Produces("application/xml")
-    public String getXml() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    @Produces("application/json")
+    public Response getAll() {
+        return Response.ok(productList.toJSON()).build();
     }
 
-    /**
-     * PUT method for updating or creating an instance of GenericResource
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public Response getById(@PathParam("id") int id) {
+        return Response.ok(productList.get(id).toJSON()).build();
+    }
+
+    @POST
+    @Consumes("application/json")
+    public Response add(JsonObject json) {
+        Response response;
+        try{
+            productList.add(new Product(json));
+            response = Response.ok(productList.get(json.getInt("productID")).toJSON()).build();
+        } catch(Exception ex){
+            response = Response.status(500).build();
+        }
+        return response;
+    }
+
     @PUT
-    @Consumes("application/xml")
-    public void putXml(String content) {
+    @Path("{id}")
+    @Consumes("application/json")
+    public Response set(@PathParam("id") int id, JsonObject json) {
+        Response response;
+        try{
+            Product p = new Product(json);
+            productList.set(id, p);
+            response = Response.ok(productList.get(id).toJSON()).build();
+        } catch(Exception ex){
+            response = Response.status(500).build();
+        }
+        return response;
+    }
+    
+    
+    @DELETE
+    @Path("{id}")
+    public Response delete(@PathParam("id") int id) {
+        Response response;
+        try{
+            productList.remove(id);
+            response = Response.ok("Deleted product with id "+id).build();
+        } catch(Exception ex){
+            response = Response.status(500).build();
+        }
+        return response;
     }
 }
